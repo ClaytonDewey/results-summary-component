da5b3f34693a5f6fd37a2b476af55273e53a2d6b
ClaytonDewey <clay@dryadmedia.com>
2023-02-21 15:37:40 -0600

    Calculate score total from data

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Score from './Score';
 
-type ResultCardProps = {};
+type ResultCardProps = {
+  data: {
+    category: string;
+    score: number;
+    icon: string;
+  }[];
+};
 
-const ResultCard: React.FC<ResultCardProps> = () => {
+const ResultCard: React.FC<ResultCardProps> = ({ data }) => {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    let total = 0;
+    const scoreTotal = data.map((item) => item.score);
+    total = Math.round(scoreTotal.reduce((a, b) => a + b) / scoreTotal.length);
+    setScore(total);
+  }, [data]);
   return (
     <div className='results'>
       <h1>Your Result</h1>
-      <Score score={76} />
+      <Score score={score} />
       <p>Great</p>
       <p>
         You scored higher than 65% of the people who have taken these tests.
