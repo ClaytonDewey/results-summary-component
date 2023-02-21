6dd6a8cb14c10550c06439b34e55dea36af51de5
ClaytonDewey <clay@dryadmedia.com>
2023-02-21 12:17:50 -0600

    Create a ResultCard component

---
+++ src/components/ResultCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Score from './Score';
+
+type ResultCardProps = {};
+
+const ResultCard: React.FC<ResultCardProps> = () => {
+  return (
+    <div className='results'>
+      <h1>Your Result</h1>
+      <Score />
+      <p>Great</p>
+      <p>
+        You scored higher than 65% of the people who have taken these tests.
+      </p>
+    </div>
+  );
+};
+export default ResultCard;
