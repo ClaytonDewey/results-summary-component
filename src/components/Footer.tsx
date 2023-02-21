df0ddfa49890b88ddecd700026f55dac9c9d5c17
ClaytonDewey <clay@dryadmedia.com>
2023-02-21 10:22:01 -0600

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      <div className='attribution'>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          rel='noreferrer'
+          target='_blank'
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a href='https://www.claytondewey.io/' rel='noreferrer' target='_blank'>
+          Clayton Dewey
+        </a>
+        .
+      </div>
+    </>
+  );
+};
+export default Footer;
