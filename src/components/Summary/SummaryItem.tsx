fe818c2f13462590a38ca5b1957983968fe97383
ClaytonDewey <clay@dryadmedia.com>
2023-02-21 13:56:17 -0600

    Update SummaryItem component

--- src/components/Summary/SummaryItem.tsx
+++ src/components/Summary/SummaryItem.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-type SummaryItemProps = {};
+type SummaryItemProps = {
+  category: string;
+  score: number;
+  icon: string;
+};
 
-const SummaryItem: React.FC<SummaryItemProps> = () => {
-  return <div>I am one item</div>;
+const SummaryItem: React.FC<SummaryItemProps> = ({ category, score, icon }) => {
+  return (
+    <div className={`summary-item ${category.toLowerCase()}`}>
+      <div className='category'>
+        <img src={icon} alt={category} />
+        {category}
+      </div>
+      <div className='score'>
+        <span>{score}</span> / 100
+      </div>
+    </div>
+  );
 };
 export default SummaryItem;
