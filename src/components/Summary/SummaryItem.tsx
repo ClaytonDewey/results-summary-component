28350f6708687ef7fa6095c9a666c4d8bee37c5b
ClaytonDewey <clay@dryadmedia.com>
2023-02-21 12:18:56 -0600

    Create SummaryItem component

---
+++ src/components/Summary/SummaryItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type SummaryItemProps = {};
+
+const SummaryItem: React.FC<SummaryItemProps> = () => {
+  return <div>I am one item</div>;
+};
+export default SummaryItem;
